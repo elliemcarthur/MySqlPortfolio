3388675320ebc23a058b9b50a670132f05d8bdbe
elliemcarthur <eleanormcarthur@gmail.com>
2018-07-12 06:31:45 -0700

    Add files via upload

---
+++ OracleHomework4/OracleHomework4_4.sql
@@ -0,0 +1,10 @@
+insert into students (First_Name,Last_Name,E_Mail,
+Address,State,Pin,
+Phone_No,S_ID,Date_Of_Birth,
+Qualification,Percent_10, Percent_12,Date_Of_Passing_12,
+Subjects)
+values ('Ellie','McArthur','eleanormcarthur@gmail.com',
+'21 DeSales','NY',7000110,
+9876575505,'S1007','1983-09-06', 'BE', 70, 69, '2002-03-30', 'Mechanical Engg.');
+
+
