04f68b95ca1f0aa4978df29d066ebd90f97bb136
elliemcarthur <eleanormcarthur@gmail.com>
2018-07-12 06:51:56 -0700

    Update oracle5_q2.sql

--- OracleHomework5/oracle5_q2.sql
+++ OracleHomework5/oracle5_q2.sql
@@ -1,16 +1,16 @@
 DECLARE
-    counter NUMBER;
+    remainder NUMBER;
     k NUMBER;
 BEGIN
   FOR n IN 1..100 LOOP   
-    counter := 0;
+    remainder := 0;
     k := floor(n/2);
     FOR i IN 2..k LOOP
         IF (mod(n, i) = 0 ) THEN
-            counter := 1;
+            remainder := 1;
         END IF;
     END LOOP;
-    IF (counter = 0) THEN
+    IF (remainder = 0) THEN
        DBMS_OUTPUT.PUT_LINE(n);
     END IF;
   END LOOP;
